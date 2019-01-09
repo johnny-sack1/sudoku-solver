c665683fcebb04dc6f4bf14005f361dc70ec8c37
johnny-sack1 <jonymaka@gmail.com>
2019-01-09 11:06:13 +0100

    Implement solver algorithm

---
+++ src/main/java/com/codecool/Solver.java
@@ -0,0 +1,52 @@
+package com.codecool;
+
+import java.util.concurrent.Callable;
+
+final class Solver implements Callable<Boolean> {
+    private final Grid grid;
+    private int numberOfSteps;
+
+    public Solver(Grid grid) {
+        this.grid = grid;
+    }
+
+    @Override
+    public Boolean call() {
+        return runSolver(0);
+    }
+
+    private boolean runSolver(int cell) {
+        synchronized (grid) {
+            int GRID_SIZE = 9;
+            if (cell == (GRID_SIZE * GRID_SIZE)) {
+                return true;
+            }
+
+            int row = cell / GRID_SIZE;
+            int column = cell % GRID_SIZE;
+
+            if (grid.getValue(row, column) != 0) {
+                return runSolver(cell + 1);
+            }
+
+            for (int value = 1; value <= GRID_SIZE; value++) {
+                if (grid.isGridValid(row, column, value)) {
+                    grid.setValue(row, column, value);
+                    numberOfSteps++;
+
+                    if (runSolver(cell + 1)) {
+                        return true;
+                    }
+
+                    grid.setValue(row, column, 0);
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public void displayNumberOfSteps() {
+        System.out.println("It took " + numberOfSteps + " steps to solve this puzzle. \n");
+    }
+}
