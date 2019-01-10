26d999cf6de644e44f80ee32f1945a4018525c7d
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2019-01-10 14:33:40 +0100

    Refactor ApiController

--- src/main/java/com/codecool/sudoku/controller/ApiController.java
+++ src/main/java/com/codecool/sudoku/controller/ApiController.java
@@ -5,18 +5,19 @@ import com.codecool.sudoku.service.FileParser;
 import com.codecool.sudoku.service.SudokuSolverService;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ApiController {
 
     private SudokuSolverService solverService;
-    //private FileParser parser;
 
     @Autowired
     public ApiController(SudokuSolverService solverService) {
@@ -27,7 +28,12 @@ public class ApiController {
     public Grid setGrid(@RequestParam("file") MultipartFile file) throws IOException {
         String fileContent = new String(file.getBytes());
         Grid grid = FileParser.getStartingGrid(fileContent);
-        this.solverService.setGrid(grid);
+        try {
+            this.solverService.setGrid(grid);
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR, "Your sudoku board is invalid.");
+        }
         return this.solverService.getGrid();
     }
 
