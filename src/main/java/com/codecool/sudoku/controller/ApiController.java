59f7db273f14c5797a3963fcb88b6838c70a7543
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2019-01-10 13:35:04 +0100

    Implement sudoku file handling

--- src/main/java/com/codecool/sudoku/controller/ApiController.java
+++ src/main/java/com/codecool/sudoku/controller/ApiController.java
@@ -1,7 +1,9 @@
 package com.codecool.sudoku.controller;
 
 import com.codecool.sudoku.model.Grid;
+import com.codecool.sudoku.service.FileParser;
 import com.codecool.sudoku.service.SudokuSolverService;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class ApiController {
 
     private SudokuSolverService solverService;
+    //private FileParser parser;
 
     @Autowired
     public ApiController(SudokuSolverService solverService) {
@@ -21,49 +24,19 @@ public class ApiController {
     }
 
     @PostMapping(value = "api/grid")
-    public String setGrid(@RequestParam("file") MultipartFile file) {
-        // manipulate file
-        return "";
+    public Grid setGrid(@RequestParam("file") MultipartFile file) throws IOException {
+
+        String fileContent = new String(file.getBytes());
+        Grid grid = (new FileParser(fileContent)).getStartingGrid();
+        this.solverService.setGrid(grid);
+        Grid grid1 = this.solverService.getGrid();
+        System.out.println(grid1);
+        return grid1;
     }
 
     @GetMapping(value = "api/grid", produces = MediaType.APPLICATION_JSON_VALUE)
     public Grid getGrid() {
-        return this.solverService.solveSudoku();
+        this.solverService.solvePuzzle();
+        return this.solverService.getGrid();
     }
-
-    //        System.out.println("THREADS: " + NUMOFTHREADS);
-    //        long start = System.nanoTime();
-    //        Grid grid= new Grid();
-    //        grid.setValue(0, 3, 2);
-    //        grid.setValue(0, 7, 3);
-    //        grid.setValue(0, 8, 1);
-    //        grid.setValue(1, 4, 6);
-    //        grid.setValue(1, 6, 4);
-    //        grid.setValue(2, 0, 2);
-    //        grid.setValue(2, 1, 8);
-    //        grid.setValue(2, 3, 1);
-    //        grid.setValue(3, 0, 5);
-    //        grid.setValue(3, 2, 2);
-    //        grid.setValue(3, 4, 4);
-    //        grid.setValue(3, 7, 7);
-    //        grid.setValue(4, 0, 4);
-    //        grid.setValue(4, 8, 2);
-    //        grid.setValue(5, 1, 9);
-    //        grid.setValue(5, 4, 1);
-    //        grid.setValue(5, 6, 8);
-    //        grid.setValue(5, 8, 3);
-    //        grid.setValue(6, 5, 1);
-    //        grid.setValue(6, 7, 9);
-    //        grid.setValue(6, 8, 6);
-    //        grid.setValue(7, 2, 4);
-    //        grid.setValue(7, 4, 8);
-    //        grid.setValue(8, 0, 1);
-    //        grid.setValue(8, 1, 5);
-    //        grid.setValue(8, 5, 6);
-    //
-    //        solvePuzzle(grid);
-    //
-    //        grid.printGrid();
-    //        long end = System.nanoTime();
-    //        System.out.println((end - start) * 1e-6);
 }
