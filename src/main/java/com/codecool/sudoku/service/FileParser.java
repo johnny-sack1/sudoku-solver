4cf8dbe129aa1e5878d2e4353203e7672d9c35a6
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2019-01-10 13:37:40 +0100

    Refactor FileParser

--- src/main/java/com/codecool/sudoku/service/FileParser.java
+++ src/main/java/com/codecool/sudoku/service/FileParser.java
@@ -1,25 +1,24 @@
 package com.codecool.sudoku.service;
 
 import com.codecool.sudoku.model.Grid;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class FileParser {
-    private String path;
+    private String string;
 
-    public FileParser(String path) {
-        this.path = path;
+    public FileParser(String s) {
+        this.string = s;
     }
 
     private List<Integer> getParsedNumbers() throws IOException {
         List<String> strList = new ArrayList<>();
-        FileReader reader = new FileReader(path);
+        StringReader reader = new StringReader(this.string);
         BufferedReader br = new BufferedReader(reader);
         String line = br.readLine();
         while (line != null) {
@@ -37,8 +36,7 @@ public class FileParser {
         List<Integer> parsedNumbers = new ArrayList<>();
         try {
             parsedNumbers = getParsedNumbers();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         Grid grid = new Grid();
