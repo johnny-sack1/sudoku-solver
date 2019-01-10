699378aa9b9bb289dfe2cdfa821fead513df41e6
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2019-01-10 11:28:10 +0100

    Add upload file capabilities to front end layer

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,7 +4,7 @@ new Vue({
   el: '#app',
 
   data: {
-    message: 'Hello Vue!', grid: [], time: 0
+    message: 'Hello Vue!', grid: [], time: 0, file: ''
   }, computed: {
 
     timeInMs() {
@@ -13,7 +13,34 @@ new Vue({
   },
 
   methods: {
-    getGrid() {
+    handleFileUpload() {
+      /*
+                Initialize the form data
+            */
+      let formData = new FormData();
+
+      /*
+          Add the form data we need to submit
+      */
+      formData.append('file', this.file);
+
+      /*
+        Make the request to the POST /single-file URL
+      */
+      axios.post('/api/grid', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then((response) => {
+        console.log(response.data);
+        console.log('SUCCESS!!');
+      })
+        .catch(() => {
+          console.log('FAILURE!!');
+      });
+    }, submitFile() {
+      this.file = this.$refs.file.files[0];
+    }, getGrid() {
       axios.get('/api/grid')
           .then((response) => {
             console.log(response);
